0b448aad5c85912c9e4c34a91da107df3fe25254
leelabakuri <leelaradhikabakuri@gmail.com>
2024-10-30 15:37:59 +0530

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,26 @@
+include <stdio.h>
+void main()
+{
+    int a,b;
+    char op;
+  printf("enter the values of a,b,op:");
+  scanf("%d%d %c",&a,&b,&op);
+  switch(op)
+  {
+      case'+':
+      printf("the addition is %d",a+b);
+      break;
+      case'-':
+    printf("the subtraction is %d",a-b);
+      break;
+      case'*':
+    printf("the multiplication is %d",a*b);
+      break;
+      case'/':
+     printf("the modulus is %d",a/b);
+      break;
+      case'%':
+     printf("the division is %d",a*b);
+      break;
+  } 
+}
